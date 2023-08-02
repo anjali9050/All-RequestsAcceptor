52dc94336bed088263fda4ef5836f539fe14eee5
anjali9050 <120652358+anjali9050@users.noreply.github.com>
2023-08-02 13:43:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,55 +9,38 @@ logging.basicConfig(level=logging.ERROR)
 
 CHANNELS = [int(CHANNEL) for CHANNEL in environ.get("CHANNELS", None).split()]       
 AuthChat = filters.chat(CHANNELS) if CHANNELS else (filters.group | filters.channel)         
-User     = Client(name = "AcceptUser", api_id=environ.get("API_ID"), api_hash=environ.get("API_HASH"), session_string = environ.get("SESSION"))
-User1     = Client(name = "Accept", api_id=environ.get("API_ID"), api_hash=environ.get("API_HASH"), session_string = environ.get("SESSION1"))
+User     = Client(name = "AcceptUser", session_string = environ.get("SESSION"))
 
-@User.on_message(filters.command(["run", "approve", "start"], [".", "/"]) & filters.private)                     
+
+@User.on_message(filters.command(["run", "approve", "start"], [".", "/"]) & AuthChat)                     
 async def approve(client: User, message: Message):
-    Id = -1001845218819
-    NJ = message.from_user.id
+    Id = message.chat.id
     await message.delete(True)
  
     try:
        while True: # create loop is better techniq 🙃
            try:
-               await User.approve_all_chat_join_requests(Id) 
-               await User1.approve_all_chat_join_requests(Id)
+               await client.approve_all_chat_join_requests(Id)         
            except FloodWait as t:
                asyncio.sleep(t.value)
-               await User.approve_all_chat_join_requests(Id) 
-               await User1.approve_all_chat_join_requests(Id) 
+               await client.approve_all_chat_join_requests(Id) 
            except Exception as e:
                logging.error(str(e))
     except FloodWait as s:
         asyncio.sleep(s.value)
         while True:
            try:
-               await User.approve_all_chat_join_requests(Id)     
-               await User1.approve_all_chat_join_requests(Id) 
+               await client.approve_all_chat_join_requests(Id)         
            except FloodWait as t:
                asyncio.sleep(t.value)
-               await User.approve_all_chat_join_requests(Id) 
-               await User1.approve_all_chat_join_requests(Id) 
+               await client.approve_all_chat_join_requests(Id) 
            except Exception as e:
                logging.error(str(e))
 
-    msg = await client.send_message(NJ, "**Task Completed** ✓ **Approved Pending All Join Request**")
+    msg = await client.send_message(Id, "**Task Completed** ✓ **Approved Pending All Join Request**")
     await asyncio.sleep(3)
     await msg.delete()
 
 
 logging.info("Bot Started....")
-apps = [User,User1]
-    for app in apps:
-        app.start()
-    idle()
-    for app in apps:
-        app.stop()
-
-
-
-
-
-
-
+User.run()
